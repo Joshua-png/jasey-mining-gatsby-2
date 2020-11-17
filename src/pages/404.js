6f138ed51e454d149e610907fc041003c473c893
RDjarbeng <richarddjarbeng@gmail.com>
2020-11-17 15:24:23 +0000

    Changed text strings

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import { Container } from '@components/global';
 const NotFoundPage = () => (
   <Layout>
     <Container>
-      <h1>NOT FOUND</h1>
+      <h1>PAGE NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Container>
   </Layout>
